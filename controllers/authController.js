e11e03058df7e3b2b081200d1c7e7c1b44c30e1e
saba-2002 <saba.khan@appinventiv.com>
2024-11-10 22:34:59 +0530

    first commit

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+exports.login = async (req, res) => {
+  const { username, password } = req.body;
+  
+  const user = await User.findOne({ username });
+  
+  if (!user || !(await bcrypt.compare(password, user.password))) {
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+  
+  res.json({ token });
+};
